0ffa7921861321b525a4698ef38008304cc3e109
Jonathan Hollander <jon@jhollander.me>
2025-03-23 17:41:41 +0000

    Improve install script

--- install.zsh
+++ install.zsh
@@ -1,31 +1,42 @@
 #!/usr/bin/env zsh
 
 function createlink {
+  # If already a symlink, skip
+  if [[ -L $HOME/$1 ]] then
+    echo "SKIPPING: $HOME/$1 (symlink exists)"
+    return;
+  fi
+  # If path doesn't exist, easy case
   if [[ ! -e $HOME/$1 ]] then
-    if ln -s ./src/$1 $HOME/$1; then
-    echo "$1 -> $HOME/$1";
+    if ln -s ${0:A:h}/src/$1 $HOME/$1; then
+      echo "${0:A:h}/src/$1 -> $HOME/$1";
     else
       echo "Failed to create symlink for $HOME/$1"
     fi
-  else
-    if [[ -d $HOME/$1 ]] then
-      print -n "Directory exists $HOME/$1. Overwrite? (y/n) "
-      read -q answer
-      if [[ $answer == [yY] ]]; then
-        echo "\nOverwriting"
-        ln -sf ./src/$1 $HOME/$1
-      else
-        echo "\nSkipping"
-      fi
+    return
+  fi
+  # If existing path is a directory, ask to overwrite
+  if [[ -d $HOME/$1 ]] then
+    print -n "Directory exists $HOME/$1. Overwrite? (y/n) "
+    read -q answer
+    if [[ $answer == [yY] ]]; then
+      echo "\nOverwriting"
+      rm -r $HOME/$1
+      ln -sf ${0:A:h}/src/$1 $HOME/$1
+      echo "${0:A:h}/src/$1 -> $HOME/$1";
     else
-      print -n "File exists $HOME/$1. Overwrite? (y/n) "
-      read -q answer
-      if [[ $answer == [yY] ]]; then
-        echo "\nOverwriting"
-        ln -sf ./src/$1 $HOME/$1
-      else
-        echo "\nSkipping"
-      fi
+      echo "\nSkipping"
+    fi
+  else
+    # Otherwise, ask to overwrite file
+    print -n "File exists $HOME/$1. Overwrite? (y/n) "
+    read -q answer
+    if [[ $answer == [yY] ]]; then
+      echo "\nOverwriting"
+      ln -sf ${0:A:h}/src/$1 $HOME/$1
+      echo "${0:A:h}/src/$1 -> $HOME/$1";
+    else
+      echo "\nSkipping"
     fi
   fi
 }
@@ -33,21 +44,15 @@ function createlink {
 # Install symlink for files at a depth of 1
 for file in $(find ./src -type f -d 1); do
   createlink ${file#./src/};
-  # ln -s $file $HOME/${file#./src/};
-  # echo "${file#./src/} -> $HOME/${file#./src/}";
 done
 
 # Install a symlink for directories at a depth of 1
 for dir in $(find ./src -type d -d 1); do
   if [[ $dir =~ '.*\.config$' ]] continue;
   createlink ${dir#./src/};
-  # echo "${dir#./src/} -> $HOME/${dir#./src/}";
-  # ln -s $dir $HOME/${dir#./src/};
 done;
 
 # Install a symlink for files and directories of .config
 for dir in $(find ./src/.config -d 1); do
   createlink ${dir#./src/};
-  # echo "${dir#./src/} -> $HOME/${dir#./src/}";
-  # ln -s $dir $HOME/${dir#./src/};
 done;
